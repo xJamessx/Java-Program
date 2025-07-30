1a92e2d348f6fe71af38bac3b7f5e4770855d157
James <85779896+xJamessx@users.noreply.github.com>
2025-07-30 17:11:11 +0800

    Update Song Lyrics.java
    - Added a multi-line comment description

--- Projects/Song Lyrics.java
+++ Projects/Song Lyrics.java
@@ -1,7 +1,10 @@
+/* Song Title: "Kapangyarihan"
+								Album: Pebble House, Vol. 1: Kuwaderno
+								By: Ben&Ben feat. SB19
+								Released on August 29, 2021 */
+
 public class Song Lyrics {
   public static void main(String[] args) {
-    // Song Title: 
-    // Composer: 
     System.out.println("Lorem Ipsum Dolor Sit Amet");
 
 
