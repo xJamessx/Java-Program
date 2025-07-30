7706489ca01d7258e87ce3cd88a1d800ba9cee4c
James <85779896+xJamessx@users.noreply.github.com>
2025-07-30 19:54:25 +0800

    Update kapangyarihanLyrics.java
    - Added a line break using the newline character.

--- Projects/kapangyarihanLyrics.java
+++ Projects/kapangyarihanLyrics.java
@@ -10,26 +10,31 @@ public class kapangyarihanLyrics {
 	  // [Intro: Ben&Ben]
 	  System.out.println("Akala niyo ba, ang kapangyarihan");
 	  System.out.println("Ay nasa inyo? Sino ba kayo?");
+	  System.out.println("\n");
 
 	  // [Verse 1: Ben&Ben]
 	  System.out.println("'Di naman kami nagkulang");
 	  System.out.println("Sa aming pag-uunawa");
 	  System.out.println("'Di ka namin isusuka");
 	  System.out.println("Kung hindi ka pa sukdulan");
+	  System.out.println("\n");
 
 	  // [Pre-Chorus: Ben&Ben]
 	  System.out.println("Huwag niyong tapakan");
 	  System.out.println("Ang katarungan");
+	  System.out.println("\n");
 
 	  // [Chorus: Ben&Ben]
 	  System.out.println("Akala niyo ba, ang kapangyarihan");
 	  System.out.println("Ay nasa inyo? Sino ba kayo?");
 	  System.out.println("Magwawagi ang katotohanan");
 	  System.out.println("Ang kadiliman ay ibabagsak");
+	  System.out.println("\n");
 
 	  // [Post-Chorus: Ben&Ben]
 	  System.out.println("Nagsisilbi ka dapat");
 	  System.out.println("Nagsisilbi ka dapat");
+	  System.out.println("\n");
 
 	  // [Verse 2: Ken, Pablo, Both (SB19)]
 	  System.out.println("'Di ba? Dapat ikaw ang nagsisilbi sa'min");
@@ -38,6 +43,7 @@ public class kapangyarihanLyrics {
 	  System.out.println("Pipilitin na pasanin hanggang sa mga mata niyo'y mamulat");
 	  System.out.println("Magising sa sigaw na sumasalamin sa sumpa niyong magsilbi sa amin");
 	  System.out.println("Tuluyang nawalan ng bisa");
+	  System.out.println("\n");
 
 	  // [Verse 3: Justin, Josh (SB19)]
 	  System.out.println("Ba't sinaulo't 'di sinapuso bilang ng buhay ng tao");
@@ -46,32 +52,38 @@ public class kapangyarihanLyrics {
 	  System.out.println("Namin ang inyong trono");
 	  System.out.println("Nadidinig niyo pa ba 'ko?");
 	  System.out.println("Pangako'y napako dahil sa korona sa ulo niyo");
+	  System.out.println("\n");
 
 	  // [Bridge: Stell (SB19)]
 	  System.out.println("'Di na magpapaalipin pa");
 	  System.out.println("Sa mga rehas mo");
 	  System.out.println("'Di mabubuwag ng kahit anumang sindak");
 	  System.out.println("Ang katotohanang 'to");
+	  System.out.println("\n");
 
 	  // [Breakdown: Ben&Ben]
 	  System.out.println("Ibon na malayang lumipad");
 	  System.out.println("'Di mo na makukulong");
+	  System.out.println("\n");
 
 	  // [Chorus: Ben&Ben & SB19]
 	  System.out.println("Akala niyo ba, ang kapangyarihan");
 	  System.out.println("Ay nasa inyo? Sino ba kayo?");
 	  System.out.println("Magwawagi ang katotohanan");
 	  System.out.println("Ang kadiliman ay ibabagsak");
+	  System.out.println("\n");
 
 	  // [Chorus: Ben&Ben & SB19]
 	  System.out.println("Akala niyo ba, ang kapangyarihan");
 	  System.out.println("Ay nasa inyo? Sino ba kayo?");
 	  System.out.println("Magwawagi ang katotohanan");
 	  System.out.println("Ang kadiliman ay ibabagsak");
+	  System.out.println("\n");
 
 	  // [Post-Chorus: Ben&Ben]
 	  System.out.println("Nagsisilbi ka dapat");
 	  System.out.println("Nagsisilbi ka dapat");
+	  System.out.println("\n");
 
 	  // [Outro: Ben&Ben]
 	  System.out.println("Katotohanan, ang dapat mamuno (Nagsisilbi ka dapat)");
