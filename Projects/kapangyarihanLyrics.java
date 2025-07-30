609847564d9318681bee38b143f30c2164884955
James <85779896+xJamessx@users.noreply.github.com>
2025-07-30 19:46:10 +0800

    Update and rename Song Lyrics.java to kapangyarihanLyrics.java
    - Renamed "Song Lyrics.java" to kapangyarihanLyrics.java."
    - Revised the multi-line comment and added other details about the song.
    - Renamed the public class to "kapangyarihanLyrics" to match with the source file name.
    - Added the full lyrics; separating the song structure to individual intro, verses, pre-chorus, choruses, post-choruses, bridge, breakdown, and outro.

---
+++ Projects/kapangyarihanLyrics.java
@@ -0,0 +1,84 @@
+/* Title: "Kapangyarihan"
+   Artists: Ben&Ben feat. SB19
+   Album: Pebble House, Vol. 1: Kuwaderno 
+   Language: Filipino
+   Genre: Folk Rock, Hip-hop
+   Release Date: August 29, 2021 */
+
+public class kapangyarihanLyrics {
+  public static void main(String[] args) {
+	  // [Intro: Ben&Ben]
+	  System.out.println("Akala niyo ba, ang kapangyarihan");
+	  System.out.println("Ay nasa inyo? Sino ba kayo?");
+
+	  // [Verse 1: Ben&Ben]
+	  System.out.println("'Di naman kami nagkulang");
+	  System.out.println("Sa aming pag-uunawa");
+	  System.out.println("'Di ka namin isusuka");
+	  System.out.println("Kung hindi ka pa sukdulan");
+
+	  // [Pre-Chorus: Ben&Ben]
+	  System.out.println("Huwag niyong tapakan");
+	  System.out.println("Ang katarungan");
+
+	  // [Chorus: Ben&Ben]
+	  System.out.println("Akala niyo ba, ang kapangyarihan");
+	  System.out.println("Ay nasa inyo? Sino ba kayo?");
+	  System.out.println("Magwawagi ang katotohanan");
+	  System.out.println("Ang kadiliman ay ibabagsak");
+
+	  // [Post-Chorus: Ben&Ben]
+	  System.out.println("Nagsisilbi ka dapat");
+	  System.out.println("Nagsisilbi ka dapat");
+
+	  // [Verse 2: Ken, Pablo, Both (SB19)]
+	  System.out.println("'Di ba? Dapat ikaw ang nagsisilbi sa'min");
+	  System.out.println("Bakit tila kami'y nananaginip nang gising");
+	  System.out.println("Ang bawat pighati na dala, dagdag bigat damdamin");
+	  System.out.println("Pipilitin na pasanin hanggang sa mga mata niyo'y mamulat");
+	  System.out.println("Magising sa sigaw na sumasalamin sa sumpa niyong magsilbi sa amin");
+	  System.out.println("Tuluyang nawalan ng bisa");
+
+	  // [Verse 3: Justin, Josh (SB19)]
+	  System.out.println("Ba't sinaulo't 'di sinapuso bilang ng buhay ng tao");
+	  System.out.println("Kayong mga hari-harian pagkalaki na ng mga ulo");
+	  System.out.println("'Wag niyong kalilimutan na hawak-hawak");
+	  System.out.println("Namin ang inyong trono");
+	  System.out.println("Nadidinig niyo pa ba 'ko?");
+	  System.out.println("Pangako'y napako dahil sa korona sa ulo niyo");
+
+	  // [Bridge: Stell (SB19)]
+	  System.out.println("'Di na magpapaalipin pa");
+	  System.out.println("Sa mga rehas mo");
+	  System.out.println("'Di mabubuwag ng kahit anumang sindak");
+	  System.out.println("Ang katotohanang 'to");
+
+	  // [Breakdown: Ben&Ben]
+	  System.out.println("Ibon na malayang lumipad");
+	  System.out.println("'Di mo na makukulong");
+
+	  // [Chorus: Ben&Ben & SB19]
+	  System.out.println("Akala niyo ba, ang kapangyarihan");
+	  System.out.println("Ay nasa inyo? Sino ba kayo?");
+	  System.out.println("Magwawagi ang katotohanan");
+	  System.out.println("Ang kadiliman ay ibabagsak");
+
+	  // [Chorus: Ben&Ben & SB19]
+	  System.out.println("Akala niyo ba, ang kapangyarihan");
+	  System.out.println("Ay nasa inyo? Sino ba kayo?");
+	  System.out.println("Magwawagi ang katotohanan");
+	  System.out.println("Ang kadiliman ay ibabagsak");
+
+	  // [Post-Chorus: Ben&Ben]
+	  System.out.println("Nagsisilbi ka dapat");
+	  System.out.println("Nagsisilbi ka dapat");
+
+	  // [Outro: Ben&Ben]
+	  System.out.println("Katotohanan, ang dapat mamuno (Nagsisilbi ka dapat)");
+	  System.out.println("Sa namumuno sa mamamayan (Nagsisilbi ka dapat)");
+	  System.out.println("Makatarungan ang dapat mamuno (Nagsisilbi ka dapat)");
+	  System.out.println("Sa namumuno sa mamamayan (Nagsisilbi ka dapat)");
+	  System.out.println("Nagsisilbi ka dapat");
+	  System.out.println("Nagsisilbi ka dapat");
+  }
+}
