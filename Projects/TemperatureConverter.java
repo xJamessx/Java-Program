1777a3e35cf630bc9de91d86e1b1083b8273493e
James <85779896+xJamessx@users.noreply.github.com>
2025-08-18 19:21:17 +0800

    Uploaded TemperatureConverter.java

---
+++ Projects/TemperatureConverter.java
@@ -0,0 +1,147 @@
+// This is to convert your Celsius (°C) reading into Celsius (°C). Given that; ((C * 9/5) + 32)
+/* import java.util.Scanner;
+    public class TemperatureConverter {
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+
+            System.out.print("Please input your temperature reading in Celsius (°C): ");
+            double celsius = input.nextDouble();
+
+            System.out.println("Your temperature reading in Fahreinheit (°F) is: " + ((celsius * 1.8) + 32) + "°F");
+            input.close();
+        }
+} */
+
+
+// This is another iteration to convert your Celsius (°C) reading into Celsius (°C). Given that; ((C * 9/5) + 32)
+/* import java.util.Scanner;
+public class TemperatureConverter {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please input your temperature reading in Celsius (°C): ");
+        double num1 = input.nextDouble();
+
+        double F = (num1 * 1.8) + 32;
+
+        System.out.println(F);
+    }
+} */
+
+
+// This is to convert your Fahrenheit (°F) reading into Celsius (°C). Given that; ((F - 32) / (9/5)
+/* import java.util.Scanner;
+public class TemperatureConverter {
+    public static void main(String[] args) {
+        Scanner fahrenheitToCelsius = new Scanner(System.in);
+
+        System.out.print("Please input your temperature reading in Fahrenheit (°F): ");
+        double fahrenheit = fahrenheitToCelsius.nextDouble();
+
+        System.out.println("Your temperature reading in Celsius (°C) is: " + (fahrenheit - 32) / (9.0/5.0) + " °C");
+
+        fahrenheitToCelsius.close();
+    }
+} */
+
+// This is to calculate the quotient of a dividend and its divisor and know if it will result in an even or odd number.
+/* import java.util.Scanner;
+public class TemperatureConverter {
+    public static void main(String[] args) {
+        Scanner oddOrEven = new Scanner(System.in);
+
+        System.out.print("Please input your dividend: ");
+        double num1 = oddOrEven.nextDouble();
+
+        System.out.print("Please input your divisor: ");
+        double num2 = oddOrEven.nextDouble();
+
+        double quotient = num1 / num2;
+
+        if (quotient % 2 != 0) {
+            System.out.println("Your quotient is an odd number. The result is " + quotient + ".");
+        }
+        else if (quotient % 2 == 0) {
+            System.out.println("Your quotient is an even number. The result is " + quotient + ".");
+        }
+
+        oddOrEven.close();
+    }
+} */
+
+// This is to calculate the sum of two addends and know if it will result in an even or odd number.
+/* import java.util.Scanner;
+public class TemperatureConverter {
+    public static void main(String[] args) {
+        Scanner oddOrEven = new Scanner(System.in);
+
+        System.out.print("Please input your first addend: ");
+        double num1 = oddOrEven.nextDouble();
+
+        System.out.print("Please input your second addend: ");
+        double num2 = oddOrEven.nextDouble();
+
+        double sum = num1 + num2;
+
+        if (sum % 2 != 0) {
+            System.out.println("Your quotient is an odd number. The result is " + sum + ".");
+        }
+        else if (sum % 2 == 0) {
+            System.out.println("Your quotient is an even number. The result is " + sum + ".");
+        }
+
+        oddOrEven.close();
+    }
+} */
+
+
+// This is to calculate the difference of the minuend and its subtrahend and know if it will result in an even or odd number.
+/* import java.util.Scanner;
+public class TemperatureConverter {
+    public static void main(String[] args) {
+        Scanner oddOrEven = new Scanner(System.in);
+
+        System.out.print("Please input your minuend: ");
+        double num1 = oddOrEven.nextDouble();
+
+        System.out.print("Please input your subtrahend: ");
+        double num2 = oddOrEven.nextDouble();
+
+        double difference = num1 - num2;
+
+        if (difference % 2 != 0) {
+            System.out.println("Your difference is an odd number. The result is " + difference + ".");
+        }
+        else if (difference % 2 == 0) {
+            System.out.println("Your difference is an even number. The result is " + difference + ".");
+        }
+
+        oddOrEven.close();
+    }
+} */
+
+
+// This is to calculate the product of the multiplicand and its multiplier and know if it will result in an even or odd number.
+/* import java.util.Scanner;
+public class TemperatureConverter {
+    public static void main(String[] args) {
+        Scanner oddOrEven = new Scanner(System.in);
+
+        System.out.print("Please input your multiplicand: ");
+        double num1 = oddOrEven.nextDouble();
+
+        System.out.print("Please input your multiplier: ");
+        double num2 = oddOrEven.nextDouble();
+
+        double product = num1 * num2;
+
+        if (product % 2 != 0) {
+            System.out.println("Your difference is an odd number. The result is " + product + ".");
+        }
+        else if (product % 2 == 0) {
+            System.out.println("Your difference is an even number. The result is " + product + ".");
+        }
+
+        oddOrEven.close();
+    }
+} */
